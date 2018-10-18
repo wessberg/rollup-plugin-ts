b50683d3b69b3d98161fefb442ab4e8d74f73b12
wessberg <frederikwessberg@gmail.com>
2018-10-18 08:08:27 +0200

    feat(interface): Interfaces for Plugin input options are now exported

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export {default} from "./plugin/typescript-plugin";
 export {TypescriptPluginOptions, BrowserslistConfig, IBrowserslistPathConfig, IBrowserslistQueryConfig, ITypescriptPluginBabelOptions, ITypescriptPluginTypescriptOptions} from "./plugin/i-typescript-plugin-options";
+export {IBabelInputOptions} from "./plugin/i-babel-options";
