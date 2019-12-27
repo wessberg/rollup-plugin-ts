d66348792b8d2dab0c31ef97596c7b0ec5f48bf4
wessberg <frederikwessberg@gmail.com>
2019-12-27 14:29:24 +0100

    More work on experiment

--- src/service/transformer/declaration-bundler/util/get-type-roots.ts
+++ src/service/transformer/declaration-bundler/util/get-type-roots.ts
@@ -1,6 +1,6 @@
 import {TS} from "../../../../type/ts";
 import {sync} from "find-up";
-import {isAbsolute, join, normalize} from "../../../../util/path/path-util";
+import {isAbsolute, join, nativeNormalize, normalize} from "../../../../util/path/path-util";
 import {DEFAULT_TYPES_ROOT, NODE_MODULES} from "../../../../constant/constant";
 
 export interface GetTypeRootsOptions {
@@ -10,8 +10,8 @@ export interface GetTypeRootsOptions {
 
 export function getTypeRoots({compilerOptions, cwd}: GetTypeRootsOptions): Set<string> {
 	if (compilerOptions.typeRoots == null) {
-		const resolved = sync(`${NODE_MODULES}/${DEFAULT_TYPES_ROOT}`, {cwd, type: "directory"});
-		return resolved == null ? new Set() : new Set([resolved]);
+		const resolved = sync(nativeNormalize(`${NODE_MODULES}/${DEFAULT_TYPES_ROOT}`), {cwd, type: "directory"});
+		return resolved == null ? new Set() : new Set([normalize(resolved)]);
 	} else {
 		return new Set(compilerOptions.typeRoots.map(normalize).map(typeRoot => (isAbsolute(typeRoot) ? typeRoot : join(cwd, typeRoot))));
 	}
