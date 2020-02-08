c0b136b7af21bf0cd5087573459c65b3aab23138
Frederik Wessberg <frederikwessberg@gmail.com>
2020-02-08 11:09:28 +0100

    fix: fix bug on windows where paths weren't always normalized

--- src/util/take-bundled-filenames/take-bundled-filenames.ts
+++ src/util/take-bundled-filenames/take-bundled-filenames.ts
@@ -1,5 +1,6 @@
 import {OutputBundle} from "rollup";
 import {isOutputChunk} from "../is-output-chunk/is-output-chunk";
+import {normalize} from "../path/path-util";
 
 /**
  * Takes all filenames that has been included in the given bundle
@@ -8,9 +9,9 @@ export function takeBundledFilesNames(bundle: OutputBundle): Set<string> {
 	const bundledFilenames: Set<string> = new Set();
 	Object.values(bundle).forEach(value => {
 		if (isOutputChunk(value)) {
-			Object.keys(value.modules).forEach(fileName => bundledFilenames.add(fileName));
+			Object.keys(value.modules).forEach(fileName => bundledFilenames.add(normalize(fileName)));
 		} else {
-			bundledFilenames.add(value.fileName);
+			bundledFilenames.add(normalize(value.fileName));
 		}
 	});
 	return bundledFilenames;
