412ba38aacd13ee05d487543499fecd4dd94b8a8
Aleksandar Ružičić <aleksandar@ruzicic.info>
2023-03-27 09:14:59 +0200

    Add TypeScript 5.0.2 support

--- test/tslib.test.ts
+++ test/tslib.test.ts
@@ -3,7 +3,7 @@ import {withTypeScriptVersions} from "./util/ts-macro.js";
 import {generateRollupBundle} from "./setup/setup-rollup.js";
 import {formatCode} from "./util/format-code.js";
 
-test.serial("Will treat every file as a module with tslib. #1", withTypeScriptVersions(">=3.6"), async (t, {typescript}) => {
+test.serial("Will treat every file as a module with tslib. #1", withTypeScriptVersions(">=3.6 <5.0"), async (t, {typescript}) => {
 	const bundle = await generateRollupBundle(
 		[
 			{
@@ -22,6 +22,47 @@ test.serial("Will treat every file as a module with tslib. #1", withTypeScriptVe
 
 			tsconfig: {
 				target: "es3",
+				lib: ["esnext"]
+			}
+		}
+	);
+	const {
+		bundle: {
+			output: [file]
+		}
+	} = bundle;
+
+	t.deepEqual(
+		formatCode(file.code),
+		formatCode(`\
+				import { __awaiter, __generator } from 'tslib';
+
+				(function () { return __awaiter(void 0, void 0, void 0, function () { return __generator(this, function (_a) {
+						return [2 /*return*/];
+				}); }); })();
+			`)
+	);
+});
+
+test.serial("Will treat every file as a module with tslib. #2", withTypeScriptVersions(">=5.0"), async (t, {typescript}) => {
+	const bundle = await generateRollupBundle(
+		[
+			{
+				entry: true,
+				fileName: "index.ts",
+				text: `\
+						(async () => {})();
+					`
+			}
+		],
+		{
+			typescript,
+			rollupOptions: {
+				external: ["tslib"]
+			},
+
+			tsconfig: {
+				target: "es5",
 				lib: ["esnext"]
 			}
 		}
